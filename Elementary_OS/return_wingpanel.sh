49e9b01382e7f8ea6ba7c12ca506ee4c4196f59b
AlexDerbenew <alex.derbenew@yandex.ru>
2019-04-09 20:30:35 +0300

    Added return_wingpanel.sh

---
+++ Elementary_OS/return_wingpanel.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:yunnxx/elementary
+sudo apt install indicator-application wingpanel-indicator-ayatana rpl
+sudo rpl "OnlyShowIn=Unity;GNOME;" "OnlyShowIn=Unity;GNOME;Pantheon;" /etc/xdg/autostart/indicator-application.desktop
+sudo cp /usr/lib/systemd/user/indicator-application.service /etc/systemd/user
+sudo su -c "echo -e '[Desktop Entry]\nName[ru]=Пользовательская команда\nComment[ru]=Системный трей\nExec=systemctl --user start indicator-application.service\nIcon=application-default-icon\nX-GNOME-Autostart-enabled=true\nType=Application' >> test.txt"
+sudo rpl "padding: 0 6px;" "padding: 0 2px;" /usr/share/themes/elementary/gtk-3.0/apps.css
