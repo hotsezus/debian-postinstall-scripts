be510b5af3a2397c22f505905c8da0660da8e17b
AlexDerbenew <alex.derbenew@yandex.ru>
2019-07-31 12:47:55 +0300

    Update gestures.sh

--- Elementary_OS/gestures.sh
+++ Elementary_OS/gestures.sh
@@ -1,15 +1,19 @@
 sudo gpasswd -a $USER input
-sudo apt install git python3 python3-setuptools xdotool python3-gi libinput-tools python-gobject wmctrl
-mkdir tmp
-cd tmp
+sudo apt install -y --no-install-recommends git python3 python3-setuptools xdotool python3-gi libinput-tools python-gobject wmctrl
+cd ~
+mkdir tmp && cd tmp
+
 git clone https://github.com/bulletmark/libinput-gestures.git
+git clone https://gitlab.com/cunidev/gestures
+
 cd libinput-gestures
 sudo make install
 libinput-gestures-setup autostart
 cd ..
-git clone https://gitlab.com/cunidev/gestures
+
 cd gestures
 sudo python3 setup.py install
-cd ../../
+
+cd ~
 sudo rm -R tmp
 wget -O /home/$USER/.config/libinput-gestures.conf https://raw.githubusercontent.com/AlexDerbenew/debian_postinstall_scripts/master/Elementary_OS/libinput-gestures.conf
