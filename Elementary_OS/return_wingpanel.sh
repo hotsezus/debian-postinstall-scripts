9b63d593a6136d34a531a121cd4d0613da343e86
AlexDerbenew <alex.derbenew@yandex.ru>
2019-09-10 11:15:10 +0300

    return_wingpanel теперь фиксит бар для всех тем в системе

--- Elementary_OS/return_wingpanel.sh
+++ Elementary_OS/return_wingpanel.sh
@@ -2,5 +2,5 @@ sudo add-apt-repository ppa:yunnxx/elementary
 sudo apt install -y --no-install-recommends indicator-application wingpanel-indicator-ayatana rpl
 sudo rpl "OnlyShowIn=Unity;GNOME;" "OnlyShowIn=Unity;GNOME;Pantheon;" /etc/xdg/autostart/indicator-application.desktop
 sudo cp /usr/lib/systemd/user/indicator-application.service /etc/systemd/user
-sudo rpl "padding: 0 6px;" "padding: 0 2px;" /usr/share/themes/elementary/gtk-3.0/apps.css
+sudo rpl "padding: 0 6px;" "padding: 0 2px;" /usr/share/themes/*/*/*.css
 sudo pkill wingpanel
