9b08b30371584b773c1a2441a0c5b4a16fab5b82
AlexDerbenew <alex.derbenew@yadnex.ru>
2019-12-05 01:27:47 +0300

    Обновил фикс расстояния между индикаторами в трее

--- Elementary_OS/return_wingpanel.sh
+++ Elementary_OS/return_wingpanel.sh
@@ -2,5 +2,7 @@ sudo add-apt-repository ppa:yunnxx/elementary
 sudo apt install -y --no-install-recommends indicator-application wingpanel-indicator-ayatana rpl
 sudo rpl "OnlyShowIn=Unity;GNOME;" "OnlyShowIn=Unity;GNOME;Pantheon;" /etc/xdg/autostart/indicator-application.desktop
 sudo cp /usr/lib/systemd/user/indicator-application.service /etc/systemd/user
-sudo rpl "padding: 0 6px;" "padding: 0 2px;" /usr/share/themes/*/*/*.css
+echo '.composited-indicator {' | tee -a $HOME/.config/gtk-3.0/gtk.css
+echo '    padding: 0 2px;' | tee -a $HOME/.config/gtk-3.0/gtk.css
+echo '}' | tee -a $HOME/.config/gtk-3.0/gtk.css
 sudo pkill wingpanel
