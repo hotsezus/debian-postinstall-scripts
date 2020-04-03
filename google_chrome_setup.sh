da6a8d0a14b6618bd0d21b4f383ab3fac5c802dc
AlexDerbenew <alex.derbenew@yandex.ru>
2020-04-03 11:48:53 +0300

    Update google_chrome_setup.sh

--- google_chrome_setup.sh
+++ google_chrome_setup.sh
@@ -1,5 +1,6 @@
 echo "Google Chrome Installation"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://dl-ssl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo sh -c 'echo "\n# Google Chrome PPA" >> /etc/apt/sources.list'
+sudo sh -c 'echo "deb [arch=amd64] https://dl-ssl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable -y
