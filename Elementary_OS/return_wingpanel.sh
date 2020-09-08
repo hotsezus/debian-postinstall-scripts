ad008d43aa376aedec20bb5eefe8d659ebc6bd24
Alex Derbenev <alex.derbenew@yandex.ru>
2020-09-08 12:37:27 +0300

    Update return_wingpanel.sh

--- Elementary_OS/return_wingpanel.sh
+++ Elementary_OS/return_wingpanel.sh
@@ -3,6 +3,6 @@ sudo apt install -y --no-install-recommends indicator-application wingpanel-indi
 sudo rpl "OnlyShowIn=Unity;GNOME;" "OnlyShowIn=Unity;GNOME;Pantheon;" /etc/xdg/autostart/indicator-application.desktop
 sudo cp /usr/lib/systemd/user/indicator-application.service /etc/systemd/user
 echo '.composited-indicator {' | tee -a $HOME/.config/gtk-3.0/gtk.css
-echo '    padding: 0 6px;' | tee -a $HOME/.config/gtk-3.0/gtk.css
+echo '    padding: 0 3px;' | tee -a $HOME/.config/gtk-3.0/gtk.css
 echo '}' | tee -a $HOME/.config/gtk-3.0/gtk.css
 sudo pkill wingpanel
