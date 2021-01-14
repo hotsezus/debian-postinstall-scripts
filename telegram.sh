daae1ad6231ab20f3fce0aca9770996fbee44c03
Alex Derbenev <alex.derbenew@yandex.ru>
2021-01-14 04:02:02 +0300

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -1,3 +1,3 @@
-wget -O - https://telegram.org/dl/desktop/linux | sudo tar xJ -C /opt/
-sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
-telegram-desktop &
+wget --show-progress -qO- https://github.com/telegramdesktop/tdesktop/releases/download/v2.5.1/tsetup.2.5.1.tar.xz | sudo tar xJ -C /opt/ &&
+sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop &&
+telegram-desktop > /dev/null 2>&1
