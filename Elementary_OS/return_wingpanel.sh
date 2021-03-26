1e5956c217f617bd4befb881d3eca504524998fb
Alex Derbenev <alex.derbenew@yandex.ru>
2021-03-26 08:12:31 +0000

    Update return_wingpanel.sh

--- Elementary_OS/return_wingpanel.sh
+++ Elementary_OS/return_wingpanel.sh
@@ -9,7 +9,7 @@ if $RELEASE_VERION <> '6'; then
 	sudo dpkg -i com.github.lafydev.wingpanel-indicator-ayatana_2.0.7_odin.deb
 else
 	wget https://github.com/Lafydev/wingpanel-indicator-ayatana/raw/master/com.github.lafydev.wingpanel-indicator-ayatana_2.0.7_amd64.deb
-	sudo dpkg -i com.github.lafydev.wingpanel-indicator-ayatana_2.0ubuntu6_amd64.deb
+	sudo dpkg -i com.github.lafydev.wingpanel-indicator-ayatana_2.0.7_amd64.deb
 	sudo apt install -y --no-install-recommends indicator-application
 	echo '.composited-indicator {' | tee -a $HOME/.config/gtk-3.0/gtk.css
 	echo '    padding: 0 3px;' | tee -a $HOME/.config/gtk-3.0/gtk.css
