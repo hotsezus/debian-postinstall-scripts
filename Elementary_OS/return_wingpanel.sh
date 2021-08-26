45a81de09863fe56a84c2db5df02c78558d589fe
Alex Derbenev <alex.derbenew@yandex.ru>
2021-08-26 22:07:40 +0300

    Update return_wingpanel.sh

--- Elementary_OS/return_wingpanel.sh
+++ Elementary_OS/return_wingpanel.sh
@@ -4,11 +4,11 @@ sudo apt install -y --no-install-recommends software-properties-common libglib2.
 
 if $RELEASE_VERION <> '6'; then
 	sudo apt install -y --no-install-recommends libwingpanel-dev indicator-application
-	wget https://github.com/Lafydev/wingpanel-indicator-ayatana/raw/master/com.github.lafydev.wingpanel-indicator-ayatana_2.0.8_odin.deb
+	curl -O -J -L https://github.com/Lafydev/wingpanel-indicator-ayatana/raw/master/com.github.lafydev.wingpanel-indicator-ayatana_2.0.8_odin.deb
 	sudo dpkg -i com.github.lafydev.wingpanel-indicator-ayatana_2.0.8_odin.deb
 else
 	sudo apt install -y --no-install-recommends indicator-application libwingpanel-2.0-dev
-	wget https://github.com/Lafydev/wingpanel-indicator-ayatana/raw/master/com.github.lafydev.wingpanel-indicator-ayatana_2.0.7_amd64.deb
+	curl -O -J -L https://github.com/Lafydev/wingpanel-indicator-ayatana/raw/master/com.github.lafydev.wingpanel-indicator-ayatana_2.0.7_amd64.deb
 	sudo dpkg -i com.github.lafydev.wingpanel-indicator-ayatana_2.0.7_amd64.deb
 	echo '.composited-indicator {' | tee -a $HOME/.config/gtk-3.0/gtk.css
 	echo '    padding: 0 3px;' | tee -a $HOME/.config/gtk-3.0/gtk.css
