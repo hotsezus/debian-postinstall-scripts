4b7d78a5410c018231c03a381b558b0c66a61784
Alex Derbenev <alex.derbenew@yandex.ru>
2022-05-22 10:38:27 +0300

    Update gnome_workspace_shortcuts.sh

--- gnome_workspace_shortcuts.sh
+++ gnome_workspace_shortcuts.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 for i in {1..9}; do gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>$i']" ; done
+for i in {1..9}; do gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Super><Shift>$i']" ; done
