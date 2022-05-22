37ae4d4ab775e3dcb4bec580e6a2da512c8c1d5b
Alex Derbenev <alex.derbenew@yandex.ru>
2022-05-22 10:41:00 +0300

    Update gnome_workspace_shortcuts.sh

--- gnome_workspace_shortcuts.sh
+++ gnome_workspace_shortcuts.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+echo "Disabling [Super + 0..9] dock switch-to-application shortcuts"
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+gsettings set org.gnome.shell.keybindings switch-to-application-1 []
+gsettings set org.gnome.shell.keybindings switch-to-application-2 []
+gsettings set org.gnome.shell.keybindings switch-to-application-3 []
+gsettings set org.gnome.shell.keybindings switch-to-application-4 []
+gsettings set org.gnome.shell.keybindings switch-to-application-5 []
+gsettings set org.gnome.shell.keybindings switch-to-application-6 []
+gsettings set org.gnome.shell.keybindings switch-to-application-7 []
+gsettings set org.gnome.shell.keybindings switch-to-application-8 []
+gsettings set org.gnome.shell.keybindings switch-to-application-9 []
+
+echo "Enabling [Super + 0..9] switch-workspace shortcuts"
 for i in {1..9}; do gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>$i']" ; done
+
+echo "Enabling [Super + Shift + 0..9] move-to-workspace shortcuts"
 for i in {1..9}; do gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Super><Shift>$i']" ; done
