ed48e265c8b58cfbfede2c2c48d6739d2a26737d
Alex Derbenev <alex.derbenew@yandex.ru>
2024-02-22 13:30:53 +0300

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -1,3 +1,28 @@
-curl -s https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest | grep "tsetup.*tar.xz" | cut -d : -f 2,3 | tr -d \" | wget -qi - | sudo tar xJ -C /opt/ &&
-sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop &&
+#!/bin/bash
+MACHINE_TYPE=`uname -m`
+
+# Removing old versions
+echo 'Removing old versions'
+sudo rm -Rf /opt/telegram* > /dev/null 2>&1
+sudo rm -Rf /usr/bin/telegram > /dev/null 2>&1
+sudo rm -Rf /usr/share/applications/telegram.desktop > /dev/null 2>&1
+
+echo 'Identify Machine Type'
+# Identify Machine Type
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	wget "https://telegram.org/dl/desktop/linux" -O /tmp/telegram.tar.xz &&
+else
+	wget "https://telegram.org/dl/desktop/linux32" -O /tmp/telegram.tar.xz &&
+fi
+
+# Extract the files
+echo 'Extracting the files'
+sudo tar Jxf /tmp/telegram.tar.xz -C /opt/ &&
+sudo mv /opt/Telegram*/ /opt/telegram &&
+
+# Create a shortcut on system
+echo 'Creating symbol link'
+sudo ln -sf /opt/telegram/Telegram /usr/bin/telegram &&
+
+echo 'Installation finish'
 telegram-desktop > /dev/null 2>&1
